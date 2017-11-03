acbef1421adbce8052404034bcf00ec807538a47
issuser <issuser@ISSCR2928.stf.nus.edu.sg>
2017-11-03 15:00:26 +0800

    test with five products

--- src/test.java
+++ src/test.java
@@ -70,7 +70,15 @@ public class test {
 
 	@Test
 	public void testComputeWithTwoProductNoDiscount(){
-		fail("Not yet implemented");
+		ArrayList <Product> listOfProduct = new ArrayList();
+		listOfProduct.add(new Product("SKU00001-RE","Item 1 RE",10.0));
+		listOfProduct.add(new Product("SKU00002-RE","Item 2 RE",10.0));
+		
+		DiscountEngine e = new DiscountEngine();
+		 e.calculateItemDiscounts(listOfProduct);
+		 
+		 assertEquals(20,  e.calculateItemDiscounts(listOfProduct),0.01);
+		
 	}
 
 	@Test
@@ -90,12 +98,33 @@ public class test {
 
 	@Test
 	public void testComputeWithFiveProductOneDiscount(){
-		fail("Not yet implemented");
+		ArrayList <Product> listOfProduct = new ArrayList();
+		listOfProduct.add(new Product("SKU00001-RE","Item 1 RE",10.0));
+		listOfProduct.add(new Product("SKU00002-RE","Item 2 RE",10.0));
+		listOfProduct.add(new Product("SKU00001-RE","Item 1 RE",10.0));
+		listOfProduct.add(new Product("SKU00003-RE","Item 3 RE",10.0));
+		listOfProduct.add(new Product("SKU00004-RE","Item 4 RE",10.0));
+		
+		DiscountEngine e = new DiscountEngine();
+		 e.calculateItemDiscounts(listOfProduct);
+		 
+		 assertEquals(20*0.7+30,  e.calculateItemDiscounts(listOfProduct),0.01);
+
 	}
 
 	@Test
 	public void testComputeWithFiveProductTwoDiscount(){
-		fail("Not yet implemented");
+		ArrayList <Product> listOfProduct = new ArrayList();
+		listOfProduct.add(new Product("SKU00001-RE","Item 1 RE",10.0));
+		listOfProduct.add(new Product("SKU00002-RE","Item 2 RE",10.0));
+		listOfProduct.add(new Product("SKU00001-RE","Item 1 RE",10.0));
+		listOfProduct.add(new Product("SKU00003-RE","Item 3 RE",10.0));
+		listOfProduct.add(new Product("SKU00002-RE","Item 2 RE",10.0));
+		
+		DiscountEngine e = new DiscountEngine();
+		 e.calculateItemDiscounts(listOfProduct);
+		 
+		 assertEquals(40*0.7+10,  e.calculateItemDiscounts(listOfProduct),0.01);
 	}
 
 }
