2a182ff31103cf92c2aaeea63ebf672837fbe3d2
nesykho <33313097+nesykho@users.noreply.github.com>
2017-11-03 14:41:12 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,23 +1,73 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class test {
 
 	@Test
-	public void testProductCanBeAdded() {
+	public void testAddProductOneProduct() {
+		/*		products.add(new Product("SKU00001RE", "Agile T-Shirt - Red", 19.90));
+		products.add(new Product("SKU00001WH", "Agile T-Shirt - White", 19.90));
+		 */
+		
+		POS pos = new POS();
+	//	pos.add("SKU00001RE");
 		
 		
+		/*double finalAmt = de.tabulatePrice(itemList);
+		//assertTrue(discountPrice<nonDiscountPrice);
+		assertEquals(14,finalAmt,0.001);
+*/
+		
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddProductMultipleProduct() {
+		
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveOneProduct(){
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testProductCanBeRemoved(){
+	public void testRemoveMultipleProduct(){
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void testListProduct(){
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeWithOneProductNoDiscount(){
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeWithTwoProductNoDiscount(){
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeWithTwoProductOneDiscount(){
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeWithFiveProductOneDiscount(){
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeWithFiveProductTwoDiscount(){
 		fail("Not yet implemented");
 	}
 
