e85e3c47d980278d6255ab5751ba5b0710a72f7e
issuser <issuser@ISSCR2927.stf.nus.edu.sg>
2017-11-03 14:18:36 +0800

    test update

--- src/test.java
+++ src/test.java
@@ -5,7 +5,19 @@ import org.junit.Test;
 public class test {
 
 	@Test
-	public void test() {
+	public void testProductCanBeAdded() {
+		
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testProductCanBeRemoved(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testListProduct(){
 		fail("Not yet implemented");
 	}
 
