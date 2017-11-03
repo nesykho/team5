ae354940016053dc9eb59a1e1331a4aee0f1c776
issuser <issuser@ISSCR2925.stf.nus.edu.sg>
2017-11-03 14:38:48 +0800

    Updated implementation

--- src/DiscountEngine.java
+++ src/DiscountEngine.java
@@ -1,9 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DiscountEngine {
 
-	public double calculateItemDiscounts(List<Product> items) {
+	public double calculateItemDiscounts(List<Product> products) {
+		DiscountEngine engine = new DiscountEngine();
+		Map<String, List<Product>> buckets = engine.bucketItemsBySKU(products);
+		
+		// Calculate item discounts
+		
+		// DEBUG: Print item after bucketing
+//		engine.printItemBucketInfo(buckets);
+		
 		return 0;
 	}
 	
+	private Map<String, List<Product>> bucketItemsBySKU(List<Product> products) {
+		Map<String, List<Product>> buckets = new HashMap<String, List<Product>>();
+		for(Product product : products) {
+			String productBaseSKU = product.getBaseSKU();
+			if(productBaseSKU==null) continue;
+			
+			List<Product> skuProducts = buckets.get(productBaseSKU);
+			if(skuProducts==null) {
+				skuProducts = new ArrayList<Product>();
+				skuProducts.add(product);
+				buckets.put(productBaseSKU, skuProducts);
+			}
+			else {
+				skuProducts.add(product);
+			}
+		}
+		return buckets;
+	}
 }
