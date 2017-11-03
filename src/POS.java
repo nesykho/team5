ae27b6b35f8dd17f965f2242deb25dd685e8c55e
issuser <issuser@ISSCR2913.stf.nus.edu.sg>
2017-11-03 15:16:02 +0800

    - NA

--- src/POS.java
+++ src/POS.java
@@ -1,15 +1,19 @@
-import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class POS {
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
 		ProductCatalogue pc = new ProductCatalogue();
 		Scanner input = new Scanner(System.in);
 		List<Product> order = new ArrayList<Product>();
+		
+		DecimalFormat df = new DecimalFormat();
+		df.applyPattern("0.00");
 		
 		while(true) {
 			System.out.println("==================================================");
@@ -25,7 +29,7 @@ public class POS {
 	
 			System.out.print("  >> ");
 			int myOption = input.nextInt();
-					
+			
 			switch(myOption) {
 				case 1: 
 					System.out.println();
@@ -36,13 +40,19 @@ public class POS {
 					
 					System.out.print("  Enter SKU >> ");
 					String myAddProduct = input.next();
+					boolean addFound = false;
 					
 					for (int i = 0; i < pc.getProducts().size(); i++) {
 						if (pc.getProducts().get(i).getSKU().equalsIgnoreCase(myAddProduct.trim())) {
 							String desc = pc.getProducts().get(i).getDescription();
 							order.add(pc.getProducts().get(i));
 							System.out.println("  Product ["  + desc + "] added to order.");
+							addFound = true;
 						}
+					}
+					
+					if (!addFound) {
+						System.out.println("  Invalid item.");
 					}
 					break;
 				case 2:
@@ -54,13 +64,19 @@ public class POS {
 					
 					System.out.print("  Enter SKU >> ");
 					String myRemoveProduct = input.next();
+					boolean removeFound = false;
 					
 					for (int i = 0; i < order.size(); i++) {
 						if (order.get(i).getSKU().equalsIgnoreCase(myRemoveProduct.trim())) {
 							String desc = order.get(i).getDescription();
 							order.remove(i);
 							System.out.println("  Product ["  + desc + "] removed from order.");
+							removeFound = true;
 						}
+					}
+					
+					if (!removeFound) {
+						System.out.println("  Item not found in order.");
 					}
 					break;
 				case 3:
@@ -70,11 +86,18 @@ public class POS {
 					System.out.println("==================================================");
 					System.out.println();
 					
-					for (int i = 0; i < order.size(); i++) {
-						System.out.println("  " + (i+1) + ". " + order.get(i).getDescription());
+					if (order.size() > 0) {
+						for (int i = 0; i < order.size(); i++) {
+							System.out.println("  " + (i+1) + ".\t[" + order.get(i).getDescription() + "] ----- $" + df.format(order.get(i).getPrice()));
+						}
+					}
+					else {
+						System.out.println("  No items in order.");
 					}
 					break;
 				case 4:
+					order.clear();
+					System.out.println("  Order cleared.");
 					break;
 			}
 			
